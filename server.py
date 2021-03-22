05ddcf519cf82a0722bffe363bf1a68a707dff40
BenjamP3 <53545127+BenjamP3@users.noreply.github.com>
2021-03-21 21:59:34 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,40 @@
 # Server file
+#load additional Python module
+import socket
+import threading
+
+SERVER = socket.gethostbyname(socket.gethostname())
+PORT = 4422
+ADDRESS = (SERVER, PORT)
+LENGTH = 64
+MSG_FMT = "utf-8"
+DIS_MSG = "|STOP|"
+
+# create TCP/IP socket
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDRESS)
+
+def client_handler(connection, address):
+    print(f"|CLIENT {address}| connection started.")
+    client_alive = True
+    while (client_alive):
+        length = int(connection.recv(LENGTH).decode(MSG_FMT))
+        message = connection.recv(length).decode(MSG_FMT)
+        if (DIS_MSG == message):
+            client_alive = False
+        else:
+            print(f"|CLIENT {address}| echoed \"{message}\"")
+    print(f"|CLIENT {address}| connection terminated.")
+    connection.close()
+
+# Starts the server and receives each connection
+def start_server():
+	server.listen()
+	print("|SERVER LISTEN| waiting for connections ...")
+	while (1):
+		connection, c_address = server.accept()
+		thread = threading.Thread(target=client_handler, args=(connection, c_address))
+		thread.start()
+
+print(f"|SERVER START| starting up on {SERVER}:{PORT} ...")
+start_server()
